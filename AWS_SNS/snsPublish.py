22188d40ae0b5353a8253d8d087013ece9eac21e
ArcherHuang <mmosconii@gmail.com>
2017-08-28 16:41:53 +0800

    Add SNS Publish Code

---
+++ AWS_SNS/snsPublish.py
@@ -0,0 +1,13 @@
+# export AWS_ACCESS_KEY_ID=
+# export AWS_SECRET_ACCESS_KEY=
+# export -p
+
+import json
+import boto3
+
+sns = boto3.resource('sns', 'us-east-1')
+topic = sns.Topic('arn:aws:sns:us-east-1::')
+response = topic.publish(
+    Subject="test",
+    Message="This is a message.",
+)
